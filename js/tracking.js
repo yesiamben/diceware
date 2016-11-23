32414dc6270079dc3781c369011dc4539d8a2c2a
Glenn Rempe <glenn@rempe.us>
2016-11-23 11:17:18 -0800

    Piwik code extracted to external JS for CSP friendly

---
+++ js/tracking.js
@@ -0,0 +1,9 @@
+var _paq = _paq || []
+_paq.push(['setTrackerUrl', 'https://piwik.rempe.us/piwik.php'])
+_paq.push(['setSiteId', '1'])
+_paq.push(['setCookieDomain', '*.rempe.us'])
+_paq.push(['setDomains', ['*.rempe.us']])
+_paq.push(['setDocumentTitle', document.title])
+_paq.push(['trackPageView'])
+_paq.push(['enableLinkTracking'])
+_paq.push(['enableHeartBeatTimer'])
